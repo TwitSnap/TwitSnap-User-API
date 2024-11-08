64c6e357b4ebc6613fd22f9935e4072ee4368ead
RafaelWu <rwu@fi.uba.ar>
2024-11-08 06:55:18 -0300

    refactor: google register with firebase token

--- src/external/google_service.py
+++ src/external/google_service.py
@@ -1,6 +1,8 @@
 from utils.requester import requester
 from config.settings import logger
 
+from config.settings import FIREBASE_API_KEY
+
 
 class GoogleService:
     def __init__(
@@ -24,5 +26,12 @@ class GoogleService:
         )
         return response.json()
 
+    async def get_user_from_firebase_token(self, token):
+        url = f"https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={FIREBASE_API_KEY}"
+        response = await self.requester.post(url, json_body={"idToken": token})
+        logger.debug(
+            f"Attempt get user info from firebase token: - response: {response.text}"
+        )
+        return response.json()
 
 google_service = GoogleService()
